7dfadb62107da5f8a21996809155fa4187545dac
root <root@misc.keithrogers.co.uk>
2016-02-13 15:58:14 +0000

    Improved provision script and fixed bugs

--- packer-config/provision/web-provision.sh
+++ packer-config/provision/web-provision.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 sleep 30
-sudo -i
-/usr/bin/wget -O /tmp/puppetlabs-release-trusty.deb http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-dpkg -i /tmp/puppetlabs-release-trusty.deb
-apt-get update -y
-apt-get install puppet git-core -y
-mv /etc/puppet /etc/puppet.orig
+sudo /usr/bin/wget -O /tmp/puppetlabs-release-trusty.deb http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+sudo dpkg -i /tmp/puppetlabs-release-trusty.deb
+sudo apt-get update -y
+sudo apt-get install puppet git-core -y
+sudo mv /etc/puppet /etc/puppet.orig
 cd /etc/puppet
-git init /etc/puppet
-git remote add -f origin https://github.com/devopper/basic_stack.git
-git config core.sparseCheckout true
-echo "puppet/" >> /etc/puppet/.git/info/sparse-checkout
-git pull origin master
+sudo git init /etc/puppet
+sudo git remote add -f origin https://github.com/devopper/basic_stack.git
+sudo git config core.sparseCheckout true
+sudo echo "puppet/" >> /etc/puppet/.git/info/sparse-checkout
+sudo git pull origin master
