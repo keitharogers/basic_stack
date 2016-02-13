43276796218253aeb66fd3c18ad444992c594999
root <root@misc.keithrogers.co.uk>
2016-02-13 16:31:21 +0000

    adding stuff

--- packer-config/provision/web-provision.sh
+++ packer-config/provision/web-provision.sh
@@ -6,11 +6,10 @@ sudo dpkg -i /tmp/puppetlabs-release-trusty.deb
 sudo apt-get update -y
 sudo apt-get install puppet git-core -y
 sudo mv /etc/puppet /etc/puppet.orig
-sudo mkdir /etc/puppet
-cd /etc/puppet
-sudo git init /etc/puppet
+sudo git init /etc
 sudo git remote add -f origin https://github.com/devopper/basic_stack.git
 sudo git config core.sparseCheckout true
-sudo touch /etc/puppet/.git/info/sparse-checkout
-echo -e "puppet/" | sudo tee /etc/puppet/.git/info/sparse-checkout
+sudo touch /etc/.git/info/sparse-checkout
+echo -e "puppet/" | sudo tee /etc/.git/info/sparse-checkout
 sudo git pull origin master
+sudo puppet apply /etc/puppet/manifests/site.pp
