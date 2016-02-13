24f1488b78ffaa35684c201c5b3308444b04c54e
root <root@misc.keithrogers.co.uk>
2016-02-13 16:06:07 +0000

    fixed another bug

--- packer-config/provision/web-provision.sh
+++ packer-config/provision/web-provision.sh
@@ -11,5 +11,6 @@ cd /etc/puppet
 sudo git init /etc/puppet
 sudo git remote add -f origin https://github.com/devopper/basic_stack.git
 sudo git config core.sparseCheckout true
+sudo mkdir /etc/puppet/.git/info/
 sudo echo "puppet/" >> /etc/puppet/.git/info/sparse-checkout
 sudo git pull origin master
