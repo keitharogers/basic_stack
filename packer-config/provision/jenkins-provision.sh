f8b7295ece7dd52fea4d64cac489d9597845f095
root <root@misc.keithrogers.co.uk>
2016-02-14 18:34:41 +0000

    adding git plugin to jenkins

--- packer-config/provision/jenkins-provision.sh
+++ packer-config/provision/jenkins-provision.sh
@@ -4,7 +4,7 @@ sleep 30
 sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update -y
-sudo apt-get install openjdk-7-jre openjdk-7-jdk -y
+sudo apt-get install openjdk-7-jre openjdk-7-jdk git -y
 sudo apt-get install jenkins -y
 sudo sed -i '/exit 0/d' /etc/rc.local
 echo -e "iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080" | sudo tee -a /etc/rc.local
