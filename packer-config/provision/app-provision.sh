41becad046e61d0947db7ea912759ddf67b00b17
root <root@misc.keithrogers.co.uk>
2016-02-14 19:11:06 +0000

    adding go directory

--- packer-config/provision/app-provision.sh
+++ packer-config/provision/app-provision.sh
@@ -5,3 +5,4 @@ sudo yum -y install golang
 sed -i '/export PATH/s//export GOPATH=\$HOME\/go/' ~/.bash_profile
 echo "export PATH=\$PATH:\$GOPATH/bin" >> ~/.bash_profile
 echo "export GOBIN=\$HOME/go/bin" >> ~/.bash_profile
+mkdir -p /home/centos/go/bin
