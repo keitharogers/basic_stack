7ce16363385431a4fbe16c67acb34c3415e1a62b
root <root@misc.keithrogers.co.uk>
2016-02-14 20:20:17 +0000

    making changes to test

--- application/app.go
+++ application/app.go
@@ -8,7 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
     h, _ := os.Hostname()
-    fmt.Fprintf(w, "Hi there, This is served from %s!", h)
+    fmt.Fprintf(w, "Hi there, This is served from! %s!", h)
 }
 
 func main() {
