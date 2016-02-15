38c866dc007b305c1772c02c5a1af22adabbdbea
root <root@misc.keithrogers.co.uk>
2016-02-15 00:55:00 +0000

    tidying up puppet config

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -1,40 +1,34 @@
 class nginx {
-
-exec { 'apt-get update':
-     command => '/usr/bin/apt-get update',
-}
-
-package { 'nginx':
-     ensure => present,
-     require => Exec['apt-get update'],
-}
-
-service { 'nginx':
-        ensure => running,
-        require => Package['nginx'],
-}
-
-file { 'lb-nginx':
-     path => '/etc/nginx/sites-available/lb',
-     ensure => file,
-     require => Package['nginx'],
-     source => 'puppet:///modules/nginx/lb',
-}
-
-file { 'default-nginx-disable':
-     path => '/etc/nginx/sites-enabled/default',
-     ensure => absent,
-     require => Package['nginx'],
-}
-
-file { 'lb-nginx-enable':
-     path => '/etc/nginx/sites-enabled/lb',
-     target => '/etc/nginx/sites-available/lb',
-     ensure => link,
-     notify => Service['nginx'],
-     require => [
-               File['lb-nginx'],
-               File['default-nginx-disable'],
-       ],
-}
+	exec { 'apt-get update':
+		command => '/usr/bin/apt-get update',
+	}
+	package { 'nginx':
+		ensure => present,
+	        require => Exec['apt-get update'],
+	}
+	service { 'nginx':
+	        ensure => running,
+	        require => Package['nginx'],
+	}
+	file { 'lb-nginx':
+		path => '/etc/nginx/sites-available/lb',
+	        ensure => file,
+	        require => Package['nginx'],
+	        source => 'puppet:///modules/nginx/lb',
+	}
+	file { 'default-nginx-disable':
+	        path => '/etc/nginx/sites-enabled/default',
+	        ensure => absent,
+	        require => Package['nginx'],
+	}
+	file { 'lb-nginx-enable':
+	        path => '/etc/nginx/sites-enabled/lb',
+	        target => '/etc/nginx/sites-available/lb',
+	        ensure => link,
+	        notify => Service['nginx'],
+	        require => [
+			File['lb-nginx'],
+		        File['default-nginx-disable'],
+       		],
+	}
 }
